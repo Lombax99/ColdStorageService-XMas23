0a6f65d2c609b143b6e5d80f379cebdb294f8995
Lombax99 <lombax99.ll@gmail.com>
2023-06-21 14:57:37 +0200

    vault backup: 2023-06-21 14:57:37

---
+++ Sprint1/codice-Sprint1/temafinale23.pl
@@ -0,0 +1,7 @@
+%====================================================================================
+% temafinale23 description   
+%====================================================================================
+context(ctxcoldstoragearea, "localhost",  "TCP", "9090").
+ qactor( controller, ctxcoldstoragearea, "it.unibo.controller.Controller").
+  qactor( transport_trolley, ctxcoldstoragearea, "it.unibo.transport_trolley.Transport_trolley").
+  qactor( cold_room, ctxcoldstoragearea, "it.unibo.cold_room.Cold_room").
