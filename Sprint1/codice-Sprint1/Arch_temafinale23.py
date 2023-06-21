f2cc3aa2c32abb54ff98a10925246a4ff5ad6794
Lombax99 <lombax99.ll@gmail.com>
2023-06-21 13:44:37 +0200

    vault backup: 2023-06-21 13:44:37

--- Sprint1/codice-Sprint1/Arch_temafinale23.py
+++ Sprint1/codice-Sprint1/Arch_temafinale23.py
@@ -19,6 +19,11 @@ eventedgeattr = {
 with Diagram('temafinale23Arch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
-     with Cluster('ctxColdStorageArea', graph_attr=nodeattr):
+     with Cluster('ctxcoldstoragearea', graph_attr=nodeattr):
           controller=Custom('controller','./qakicons/symActorSmall.png')
+          controller=Custom('controller','./qakicons/symActorSmall.png')
+          transport_trolley=Custom('transport_trolley','./qakicons/symActorSmall.png')
+          cold_room=Custom('cold_room','./qakicons/symActorSmall.png')
+     controller >> Edge(color='blue', style='solid', xlabel='doJob', fontcolor='blue') >> transport_trolley
+     transport_trolley >> Edge(color='blue', style='solid', xlabel='updateWeight', fontcolor='blue') >> cold_room
 diag
--- Sprint1/codice-Sprint1/temafinale23.pl
+++ Sprint1/codice-Sprint1/temafinale23.pl
@@ -1,5 +1,8 @@
 %====================================================================================
 % temafinale23 description   
 %====================================================================================
-context(ctxcoldstoragearea, "localhost",  "TCP", "8095").
+context(ctxcoldstoragearea, "localhost",  "TCP", "9090").
  qactor( controller, ctxcoldstoragearea, "it.unibo.controller.Controller").
+  qactor( controller, ctxcoldstoragearea, "it.unibo.controller.Controller").
+  qactor( transport_trolley, ctxcoldstoragearea, "it.unibo.transport_trolley.Transport_trolley").
+  qactor( cold_room, ctxcoldstoragearea, "it.unibo.cold_room.Cold_room").
