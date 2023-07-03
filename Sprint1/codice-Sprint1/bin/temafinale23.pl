ec57144af36cd0358863cff6b5ca4cfea3b25a0c
Lombax99 <lombax99.ll@gmail.com>
2023-07-03 15:40:11 +0200

    Eliminazione codice-Sprint1

--- Sprint1/codice-Sprint1/bin/temafinale23.pl
+++
@@ -1,7 +0,0 @@
-%====================================================================================
-% temafinale23 description   
-%====================================================================================
-context(ctxcoldstoragearea, "localhost",  "TCP", "8080").
- qactor( controller, ctxcoldstoragearea, "it.unibo.controller.Controller").
-  qactor( transport_trolley, ctxcoldstoragearea, "it.unibo.transport_trolley.Transport_trolley").
-  qactor( cold_room, ctxcoldstoragearea, "it.unibo.cold_room.Cold_room").
