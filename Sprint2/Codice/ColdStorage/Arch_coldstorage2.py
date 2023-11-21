a0f5c574bdf98253354ab16fe8e96519d9470cba
LisaIU00 <126091503+LisaIU00@users.noreply.github.com>
2023-11-21 11:56:57 +0100

    grafico e qak sprint 2

--- Sprint2/Codice/ColdStorage/Arch_coldstorage2.py
+++ Sprint2/Codice/ColdStorage/Arch_coldstorage2.py
@@ -33,14 +33,14 @@ with Diagram('coldstorage2Arch', show=False, outformat='png', graph_attr=graphat
      with Cluster('ctxalarm', graph_attr=nodeattr):
           led=Custom('led(ext)','./qakicons/externalQActor.png')
           sonar=Custom('sonar(ext)','./qakicons/externalQActor.png')
-     controller >> Edge(color='magenta', style='solid', decorate='true', label='<doJob &nbsp; >',  fontcolor='magenta') >> transporttrolley
-     transporttrolley >> Edge(color='magenta', style='solid', decorate='true', label='<moverobot &nbsp; >',  fontcolor='magenta') >> robotpos
-     tickethandler >> Edge(color='magenta', style='solid', decorate='true', label='<weightrequest &nbsp; >',  fontcolor='magenta') >> coldroom
-     robotpos >> Edge(color='magenta', style='solid', decorate='true', label='<doplan &nbsp; >',  fontcolor='magenta') >> planexec
-     facade >> Edge(color='magenta', style='solid', decorate='true', label='<depositRequest &nbsp; checkmyticket &nbsp; >',  fontcolor='magenta') >> tickethandler
-     facade >> Edge(color='magenta', style='solid', decorate='true', label='<loaddone &nbsp; >',  fontcolor='magenta') >> controller
-     facade >> Edge(color='magenta', style='solid', decorate='true', label='<getweight &nbsp; >',  fontcolor='magenta') >> coldroom
-     planexec >> Edge(color='magenta', style='solid', decorate='true', label='<step &nbsp; >',  fontcolor='magenta') >> basicrobot
+     controller >> Edge(color='magenta', style='solid', decorate='true', label='<doJob<font color="darkgreen"> jobdone robotDead</font> &nbsp; >',  fontcolor='magenta') >> transporttrolley
+     transporttrolley >> Edge(color='magenta', style='solid', decorate='true', label='<moverobot<font color="darkgreen"> moverobotdone moverobotfailed</font> &nbsp; >',  fontcolor='magenta') >> robotpos
+     tickethandler >> Edge(color='magenta', style='solid', decorate='true', label='<weightrequest<font color="darkgreen"> weightOK weightKO</font> &nbsp; >',  fontcolor='magenta') >> coldroom
+     robotpos >> Edge(color='magenta', style='solid', decorate='true', label='<doplan<font color="darkgreen"> doplandone doplanfailed</font> &nbsp; >',  fontcolor='magenta') >> planexec
+     facade >> Edge(color='magenta', style='solid', decorate='true', label='<depositRequest<font color="darkgreen"> accept reject</font> &nbsp; checkmyticket<font color="darkgreen"> ticketchecked</font> &nbsp; >',  fontcolor='magenta') >> tickethandler
+     facade >> Edge(color='magenta', style='solid', decorate='true', label='<loaddone<font color="darkgreen"> chargetaken</font> &nbsp; >',  fontcolor='magenta') >> controller
+     facade >> Edge(color='magenta', style='solid', decorate='true', label='<getweight<font color="darkgreen"> currentweight</font> &nbsp; >',  fontcolor='magenta') >> coldroom
+     planexec >> Edge(color='magenta', style='solid', decorate='true', label='<step<font color="darkgreen"> stepdone stepfailed</font> &nbsp; >',  fontcolor='magenta') >> basicrobot
      controller >> Edge(color='blue', style='solid',  label='<updateWeight &nbsp; >',  fontcolor='blue') >> coldroom
      basicrobot >> Edge(color='blue', style='solid',  label='<robotready &nbsp; >',  fontcolor='blue') >> transporttrolley
      planexec >> Edge(color='blue', style='solid',  label='<nextmove &nbsp; nomoremove &nbsp; >',  fontcolor='blue') >> planexec
--- Sprint2/Codice/ColdStorage/coldstorage2.pl
+++ Sprint2/Codice/ColdStorage/coldstorage2.pl
@@ -2,27 +2,48 @@
 % coldstorage2 description   
 %====================================================================================
 request( doJob, doJob(KG) ).
+reply( jobdone, jobdone(NO_PARAM) ).  %%for doJob
+reply( robotDead, robotDead(NO_PARAM) ).  %%for doJob
 dispatch( updateWeight, updateWeight(P_EFF,P_PRO) ).
 request( depositRequest, depositRequest(PESO) ).
+reply( accept, accept(TICKET) ).  %%for depositRequest
+reply( reject, reject(NO_PARAM) ).  %%for depositRequest
 request( depositRequestF, depositRequestF(PESO) ).
+reply( acceptF, acceptF(TICKET) ).  %%for depositRequestF
+reply( rejectF, rejectF(NO_PARAM) ).  %%for depositRequestF
 request( weightrequest, weightrequest(PESO) ).
+reply( weightOK, weightOK(NO_PARAM) ).  %%for weightrequest
+reply( weightKO, weightKO(NO_PARAM) ).  %%for weightrequest
 request( checkmyticket, checkmyticket(TICKET) ).
+reply( ticketchecked, ticketchecked(BOOL) ).  %%for checkmyticket
 request( checkmyticketF, checkmyticketF(TICKET) ).
+reply( ticketcheckedF, ticketcheckedF(BOOL) ).  %%for checkmyticketF
 request( loaddone, loaddone(PESO) ).
+reply( chargetaken, chargetaken(NO_PARAM) ).  %%for loaddone
 request( loaddoneF, loaddoneF(PESO) ).
+reply( chargetakenF, chargetakenF(NO_PARAM) ).  %%for loaddoneF
 request( getweight, getweight(NO_PARAM) ).
+reply( currentweight, currentweight(PESO_EFF,PESO_PRO) ).  %%for getweight
 request( getweightF, getweightF(NO_PARAM) ).
+reply( currentweightF, currentweightF(PESO_EFF,PESO_PRO) ).  %%for getweightF
 dispatch( startToDoThings, startToDoThings(NO_PARAM) ).
 dispatch( cmd, cmd(MOVE) ).
 dispatch( end, end(ARG) ).
 request( step, step(TIME) ).
+reply( stepdone, stepdone(V) ).  %%for step
+reply( stepfailed, stepfailed(DURATION,CAUSE) ).  %%for step
 request( doplan, doplan(PATH,STEPTIME) ).
+reply( doplandone, doplandone(ARG) ).  %%for doplan
+reply( doplanfailed, doplanfailed(ARG) ).  %%for doplan
 dispatch( nextmove, nextmove(M) ).
 dispatch( nomoremove, nomoremove(M) ).
 dispatch( robotready, robotready(BOOL) ).
 dispatch( setdirection, dir(D) ).
 request( moverobot, moverobot(TARGETX,TARGETY) ).
+reply( moverobotdone, moverobotok(ARG) ).  %%for moverobot
+reply( moverobotfailed, moverobotfailed(PLANDONE,PLANTODO) ).  %%for moverobot
 request( getrobotstate, getrobotstate(ARG) ).
+reply( robotstate, robotstate(POS,DIR) ).  %%for getrobotstate
 dispatch( setrobotstate, setpos(X,Y,D) ).
 dispatch( arrivedhome, arrivedhome(NO_PARAM) ).
 dispatch( moving, moving(NO_PARAM) ).
