9b1775a93aa099946028f7e56c08febd03a88781
LisaIU00 <126091503+LisaIU00@users.noreply.github.com>
2023-12-27 15:14:33 +0100

    modifichine

---
+++ FinalVersion/ColdStorageF/test/TestServiceSprint0.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.Socket;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestService{
+
+    @Test
+    public void mainUseCaseTest(){
+        //connect to port
+        try{
+            Socket client= new Socket("localhost", 8040);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            //send message
+            out.write("msg(depositRequest,request,test,coldstorageservice,depositRequest(100),1)\n");
+            out.flush();
+            //wait for response
+            String response= in.readLine();
+            System.out.println(response);
+            assertTrue(response.contains("reject"));
+        }catch(Exception e){
+            fail();
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
---
+++ FinalVersion/ColdStorageF/test/TestServiceSprint11.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestService{
+
+    @Test
+    public void mainUseCaseTest(){
+        //connect to port
+        try{
+            Socket client= new Socket("localhost", 8040);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            //send message first client request ticket
+            out.write("msg(depositRequestF,request,test2,facade,depositRequestF(1),1)\n");
+
+            //send message second client request ticket
+            out.write("msg(depositRequestF,request,test2,facade,depositRequestF(1),1)\n");
+            out.flush();
+            //wait for response
+            String response= in.readLine();
+            response = response.split(",")[4];
+            System.out.println(response);
+
+            String result = response.split("F")[0];
+            assertTrue(result.equalsIgnoreCase("accept"));
+
+            String ticket = response.replace("acceptF(","");
+            ticket = ticket.replace(")","");
+
+
+            //Thread.sleep(800);
+            System.out.println("sleep 2 minutes");
+            TimeUnit.MINUTES.sleep(2);
+
+            //send message second client check ticket
+            out.write("msg(checkmyticketF,request,test2,facade,checkmyticketF(" + ticket + "),1)\n");
+            out.flush();
+            //wait for response
+            String response1 = in.readLine();
+            System.out.println(response1);
+
+            response1 = response1.split(",")[4];
+            String checked = response1.replace("ticketcheckedF(","");
+            checked = checked.replace(")","");
+            assertTrue(checked.equalsIgnoreCase("false"));
+
+        }catch(Exception e){
+            fail();
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
